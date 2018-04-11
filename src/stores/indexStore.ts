a40e65e394c9daafd7d84ff5512bf6f939abd71c
ly <ly1169134156@163.com>
2018-04-11 18:51:01 +0800

    部分未提交

--- src/stores/indexStore.ts
+++ src/stores/indexStore.ts
@@ -3,9 +3,26 @@ import TopicStore from './topicStore'
 import { apiTopics } from '../configs'
 import * as fetch from 'isomorphic-fetch'
 
+const asyncMethod = (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    const func = descriptor.value
+    return {
+        get() {
+            return (...args: any[]) => {
+                return Promise.resolve(func.apply(this, args)).catch(error => {
+                    console.log(error.message)
+                })
+            }
+        },
+        set(newValue: any) {
+            return newValue
+        }
+    }
+}
+
 class IndexStore {
     @observable topics: Array<TopicStore> = []
 
+    @asyncMethod
     @action
     async init(): Promise<void> {
         const request = new Request(apiTopics + '?limit=10&page=1', {
@@ -13,6 +30,7 @@ class IndexStore {
         })
         const rsp = await fetch(request)
         const rspData = await rsp.json()
+        // throw new Error('test')
         if (rspData.success) {
             runInAction(() => {
                 this.topics = rspData.data
